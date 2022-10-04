926bae3406a2af39fd6013a74e286f33773bc771
snappyboy16 <dreykfame@gmail.com>
2022-10-04 09:44:12 +0300

    three commit

--- handlers/users/ref.py
+++ handlers/users/ref.py
@@ -7,5 +7,5 @@ from loader import dp
 @dp.message_handler(commands=['ref'])
 async def bot_start(message: types.Message):
     link = 'https://t.me/dubna_hack_bot?start=' + str(message.from_user.id)
-    await message.answer(f'Твоя ссылка для приглашения: {link}\n\nТвои бонусы: 0')
+    await message.answer(f'Твоя ссылка для приглашения: {link}\n\nТвои бонусы: 0 б')
 
